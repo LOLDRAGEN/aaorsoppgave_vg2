4334fd02b57ceb769d35e926b1c82779931f7b7a
Åmund Volla <amund.volla@gmail.com>
2023-12-01 11:49:51 +0100

    :3  17 finnal?

---
+++ word_og_txt_dokumenter/users.sql
@@ -0,0 +1,6 @@
+INSERT INTO terminoppgave_vg2.users (uid, username, pwd, email, admin) VALUES (1, 'a', 'a', null, 0);
+INSERT INTO terminoppgave_vg2.users (uid, username, pwd, email, admin) VALUES (2, 'test', 'test', null, 1);
+INSERT INTO terminoppgave_vg2.users (uid, username, pwd, email, admin) VALUES (3, 'b', 'b', null, 0);
+INSERT INTO terminoppgave_vg2.users (uid, username, pwd, email, admin) VALUES (4, 'test2', 'bbb', null, 0);
+INSERT INTO terminoppgave_vg2.users (uid, username, pwd, email, admin) VALUES (8, 'jorgen', 'jorgen', 'jorgen@jorjor.jor', 0);
+INSERT INTO terminoppgave_vg2.users (uid, username, pwd, email, admin) VALUES (9, 'jor', 'jor', 'jor.@jorjor.jor', 0);
